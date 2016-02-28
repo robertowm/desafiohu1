dbdbe302b0e21e8a82c5ddc0c76a269f85df3b56
Roberto Weidmann Menezes <robertowm@gmail.com>
2016-02-28 18:57:37 -0300

    Ajusta controller com base nas mudanças no widget

--- src/main/scala/com/hotelurbano/desafiohu1/controller/WidgetController.scala
+++ src/main/scala/com/hotelurbano/desafiohu1/controller/WidgetController.scala
@@ -1,55 +1,53 @@
 package com.hotelurbano.desafiohu1.controller
 
-import com.hotelurbano.desafiohu1.repository.{HotelAvailabilityRepositoryInstance, HotelRepositoryInstance}
+import com.google.inject.Inject
+import com.hotelurbano.desafiohu1.repository.{HotelAvailabilityRepository, HotelRepository}
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
-class WidgetController extends Controller {
-
-  val hotelRepository = HotelRepositoryInstance
-  val hotelAvailabilityRepository = HotelAvailabilityRepositoryInstance
+class WidgetController @Inject()(
+                                  hotelRepository: HotelRepository,
+                                  hotelAvailabilityRepository: HotelAvailabilityRepository
+                                ) extends Controller {
 
   get("/widget") { request: Request =>
     response.ok.view("search.mustache", null)
   }
-
-  case class AutocompleteResponse(name: String, relatedField: String)
 
   get("/widget/autocomplete") { request: Request =>
     val query = request.getParam("query")
     val cities = hotelRepository.searchByCity(query)
       .map { _.city }
       .distinct
-      .map { AutocompleteResponse(_, "city") }
+
     val hotels = hotelRepository.searchByCityOrName(query)
-      .map { hotel =>
-        AutocompleteResponse(hotel.name + " - " + hotel.city, "hotel")
-      }
+      .map { h => h.name + " - " + h.city }
 
     cities ++ hotels
   }
 
   post("/search") { request: Request =>
-
-    request.params.foreach {
-      _ match {
-        case (k, v) => println(k + ":" + v)
-      }
-    }
-
     val where = request.getParam("where")
     val begin = DateTime.parse(request.getParam("begin"),  DateTimeFormat.forPattern("dd/MM/yyyy"))
-    val end =   DateTime.parse(request.getParam("end"),  DateTimeFormat.forPattern("dd/MM/yyyy"))
+    val end = DateTime.parse(request.getParam("end"),  DateTimeFormat.forPattern("dd/MM/yyyy"))
+    val responseType = request.getParam("responseType", "json")
 
-    if (request.containsParam("city")) {
-      hotelAvailabilityRepository.searchByRangeAndCity(begin, end, request.getParam("city"))
+    val result = where.split(" - ") match {
+      case Array(city) =>
+        println("city: " + city)
+        hotelAvailabilityRepository.searchByRangeAndCity(begin, end, city)
+      case Array(name , city) =>
+        println("name: " + name + ", city: " + city)
+        hotelAvailabilityRepository.searchByRangeAndNameAndCity(begin, end, name, city)
+    }
+
+    if (responseType.equals("view")) {
+      response.ok.view("result.mustache", result)
     } else {
-      hotelAvailabilityRepository.searchByRangeAndHotel(begin, end, request.getParam("hotel"))
+      result
     }
   }
-
-
 
 }
