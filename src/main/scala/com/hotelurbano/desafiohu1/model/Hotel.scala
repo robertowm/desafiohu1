bef796bc9b0fc386d50fc4a07a2b2496306808c3
Roberto Weidmann Menezes <robertowm@gmail.com>
2016-02-28 16:35:05 -0300

    Ajusta modelos para serem recuperaveis por id

--- src/main/scala/com/hotelurbano/desafiohu1/model/Hotel.scala
+++ src/main/scala/com/hotelurbano/desafiohu1/model/Hotel.scala
@@ -1,15 +1,16 @@
 package com.hotelurbano.desafiohu1.model
 
-import org.apache.lucene.document.{TextField, IntField, Field, Document}
+import org.apache.lucene.document._
 
-case class Hotel(id: Int, city: String, name: String) extends Searchable {
+case class Hotel(id: String, city: String, name: String) extends Searchable {
   def this(document: Document) =
-    this(document.get("docID").toInt, document.get("city"), document.get("name"))
+    this(document.get("id"), document.get("city"), document.get("name"))
 
   def toDocument: Document = {
     val document = new Document
 
-    document.add(new IntField("docID", id, Field.Store.YES))
+    // document.add(new IntField("id", id, Field.Store.YES))
+    document.add(new StringField("id", id, Field.Store.YES))
     document.add(new TextField("city", city, Field.Store.YES))
     document.add(new TextField("name", name, Field.Store.YES))
 
--- src/main/scala/com/hotelurbano/desafiohu1/model/HotelAvailability.scala
+++ src/main/scala/com/hotelurbano/desafiohu1/model/HotelAvailability.scala
@@ -4,18 +4,19 @@ import com.twitter.finatra.conversions.time.RichStringTime
 import org.apache.lucene.document._
 import org.joda.time.DateTime
 
-case class HotelAvailability(hotelId: Int, city: String, name: String, date: DateTime, total: Int)
+case class HotelAvailability(hotelId: String, city: String, name: String, date: DateTime, total: Int)
     extends Searchable {
 
   def this(document: Document) =
-    this(document.get("hotelId").toInt, document.get("city"), document.get("name"),
+    this(document.get("hotelId"), document.get("city"), document.get("name"),
       document.get("date").toDateTime, document.get("total").toInt)
 
   def toDocument: Document = {
     val document = new Document
     val parsedDate = DateTools.dateToString(date.toDate, DateTools.Resolution.DAY)
 
-    document.add(new IntField("hotelId", hotelId, Field.Store.YES))
+    // document.add(new IntField("hotelId", hotelId, Field.Store.YES))
+    document.add(new StringField("hotelId", hotelId, Field.Store.YES))
     document.add(new TextField("city", city, Field.Store.YES))
     document.add(new TextField("name", name, Field.Store.YES))
     document.add(new StringField("date", parsedDate, Field.Store.YES))
