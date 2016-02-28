dd7e9f801daea65edf37a28a8f9dd2641739003e
Roberto Weidmann Menezes <robertowm@gmail.com>
2016-02-28 18:59:42 -0300

    Ajusta import no AppServer

--- src/main/scala/com/hotelurbano/desafiohu1/AppServer.scala
+++ src/main/scala/com/hotelurbano/desafiohu1/AppServer.scala
@@ -1,11 +1,11 @@
 package com.hotelurbano.desafiohu1
 
-import com.hotelurbano.desafiohu1.controller.{WidgetController, PingController}
-import com.twitter.finagle.http.{Response, Request}
+import com.hotelurbano.desafiohu1.controller.{PingController, WidgetController}
+import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.CommonFilters
 import com.twitter.finatra.http.routing.HttpRouter
-import com.twitter.finatra.logging.filter.{TraceIdMDCFilter, LoggingMDCFilter}
+import com.twitter.finatra.logging.filter.{LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.logging.modules.Slf4jBridgeModule
 
 object AppServerMain extends AppServer
