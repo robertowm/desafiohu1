a8f4ba9e9fcc54013b77848e44fdb54a510899fc
Roberto Weidmann Menezes <robertowm@gmail.com>
2016-02-28 21:03:17 -0300

    Melhora organização do código para buscas relacionadas a disponibilidade de hotel e corrige as queries usadas

--- src/main/scala/com/hotelurbano/desafiohu1/repository/HotelAvailabilityRepository.scala
+++ src/main/scala/com/hotelurbano/desafiohu1/repository/HotelAvailabilityRepository.scala
@@ -2,46 +2,28 @@ package com.hotelurbano.desafiohu1.repository
 
 import com.google.inject.Inject
 import com.hotelurbano.desafiohu1.repository.index.HotelAvailabilityIndex
-import org.apache.lucene.index.Term
 import org.apache.lucene.search.BooleanClause.Occur
 import org.apache.lucene.search._
-import org.apache.lucene.util.BytesRef
 import org.joda.time.DateTime
 
 class HotelAvailabilityRepository @Inject()(index: HotelAvailabilityIndex){
 
-  private def createRangeQuery(fieldName: String, begin: DateTime, end: DateTime) : Query =
-    createRangeQuery(fieldName, begin.toString, end.toString)
-
-  private def createRangeQuery(fieldName: String, begin: String, end: String) : Query =
-    new TermRangeQuery(fieldName, new BytesRef(end), new BytesRef(end), true, true)
-
-  private def createNumericRangeQuery(fieldName: String, begin: Int, end: Int = Int.MaxValue) : Query =
-    NumericRangeQuery.newIntRange(fieldName, begin, end, true, true)
-
-  private def createPhraseQuery(fieldName: String, values: Array[String]) : Query = {
-    val builder = new PhraseQuery.Builder()
-    values.map{ new Term(fieldName,_) }
-      .foreach { builder.add(_) }
-    builder.build()
-  }
-
   def searchByRangeAndCity(begin: DateTime, end: DateTime, city: String) =
     index.search(
       new BooleanQuery.Builder()
-        .add(createRangeQuery("date", begin, end), Occur.MUST)
-        .add(createNumericRangeQuery("total", 1), Occur.MUST)
-        .add(createPhraseQuery("city", city.split(" ")), Occur.MUST)
+        .add(index.createRangeQuery("date", begin, end), Occur.MUST)
+        .add(index.createNumericRangeQuery("total", 1), Occur.MUST)
+        .add(index.createFieldQuery("city", city), Occur.MUST)
         .build()
     )
 
   def searchByRangeAndNameAndCity(begin: DateTime, end: DateTime, name: String, city: String) =
     index.search(
       new BooleanQuery.Builder()
-        .add(createRangeQuery("date", begin, end), Occur.MUST)
-        .add(createNumericRangeQuery("total", 1), Occur.MUST)
-        .add(createPhraseQuery("name", name.split(" ")), Occur.MUST)
-        .add(createPhraseQuery("city", city.split(" ")), Occur.MUST)
+        .add(index.createRangeQuery("date", begin, end), Occur.MUST)
+        .add(index.createNumericRangeQuery("total", 1), Occur.MUST)
+        .add(index.createFieldQuery("name", name), Occur.MUST)
+        .add(index.createFieldQuery("city", city), Occur.MUST)
         .build
     )
 }
--- src/main/scala/com/hotelurbano/desafiohu1/repository/index/LuceneIndex.scala
+++ src/main/scala/com/hotelurbano/desafiohu1/repository/index/LuceneIndex.scala
@@ -8,6 +8,8 @@ import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig,
 import org.apache.lucene.queryparser.classic.{MultiFieldQueryParser, QueryParser}
 import org.apache.lucene.search._
 import org.apache.lucene.store.RAMDirectory
+import org.apache.lucene.util.BytesRef
+import org.joda.time.DateTime
 
 abstract class LuceneIndex[MODEL <: Searchable] {
 
@@ -35,16 +37,9 @@ abstract class LuceneIndex[MODEL <: Searchable] {
       .map(doc => convertDocument(doc))
       .toList
 
-  def search(query: String, defaultFields: Array[String]): List[MODEL] = {
-    val parser = new MultiFieldQueryParser(defaultFields, analyzer)
-    parser.setDefaultOperator(QueryParser.Operator.OR)
-    parser.setAllowLeadingWildcard(true);
-    parser.setEnablePositionIncrements(true);
-    parser.setLowercaseExpandedTerms(true);
-    parser.setPhraseSlop(5);
+  def search(query: String, defaultFields: Array[String]): List[MODEL] =
+    this.search(createMultiFieldQuery(defaultFields, query))
 
-    this.search(parser.parse(query))
-  }
 
   def search(query: String, defaultField: String): List[MODEL] =
     this.search(query, Array(defaultField))
@@ -55,4 +50,45 @@ abstract class LuceneIndex[MODEL <: Searchable] {
       case head :: tail => Some(head)
       case _ => None
     }
+
+  def createRangeQuery(fieldName: String, begin: DateTime, end: DateTime) : Query =
+    createRangeQuery(fieldName, begin.toString, end.toString)
+
+  def createRangeQuery(fieldName: String, begin: String, end: String) : Query =
+    new TermRangeQuery(fieldName, new BytesRef(end), new BytesRef(end), true, true)
+
+  def createNumericRangeQuery(fieldName: String, begin: Int, end: Int = Int.MaxValue) : Query =
+    NumericRangeQuery.newIntRange(fieldName, begin, end, true, true)
+
+//  def createPhraseQuery(fieldName: String, values: Array[String]) : Query = {
+//    val builder = new PhraseQuery.Builder()
+//    values.map{ new Term(fieldName,_) }
+//      .foreach { builder.add(_) }
+//    builder.build()
+//  }
+
+//  def createTermQuery(fieldName: String, value: String) : Query =
+//    new TermQuery(new Term(fieldName, value))
+
+  def createMultiFieldQuery(fields: Array[String], value: String) : Query = {
+    val parser = new MultiFieldQueryParser(fields, analyzer)
+    parser.setDefaultOperator(QueryParser.Operator.OR)
+    parser.setAllowLeadingWildcard(true);
+    parser.setEnablePositionIncrements(true);
+    parser.setLowercaseExpandedTerms(true);
+    parser.setPhraseSlop(5);
+
+    parser.parse(value)
+  }
+
+  def createFieldQuery(field: String, value: String) : Query = {
+    val parser = new QueryParser(field, analyzer)
+    parser.setDefaultOperator(QueryParser.Operator.OR)
+    parser.setAllowLeadingWildcard(true);
+    parser.setEnablePositionIncrements(true);
+    parser.setLowercaseExpandedTerms(true);
+    parser.setPhraseSlop(5);
+
+    parser.parse(value)
+  }
 }
