d5ecdbe78ad87ac231ae63ddd2243565ba653b30
Roberto Weidmann Menezes <robertowm@gmail.com>
2016-02-29 00:46:20 -0300

    Adiciona novo caso de teste

---
+++ src/test/scala/com/hotelurbano/desafiohu1/repository/HotelAvailabilityRepositoryUnitTest.scala
@@ -0,0 +1,25 @@
+package com.hotelurbano.desafiohu1.repository
+
+import com.google.inject.Inject
+import com.twitter.inject.Test
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+class HotelAvailabilityRepositoryUnitTest @Inject()(
+                                                     hotelAvailabilityRepository: HotelAvailabilityRepository
+                                                   ) extends Test {
+
+  "Search" should {
+    "find a room at Mercatto Casa Hotel between 06/05/15 to 08/05/15" in {
+      val begin = DateTime.parse("06/05/2015", DateTimeFormat.forPattern("dd/MM/yyyy"))
+      val end   = DateTime.parse("08/05/2015", DateTimeFormat.forPattern("dd/MM/yyyy"))
+      val city = "Araruama"
+      val name = "Mercatto Casa Hotel"
+
+      val result = hotelAvailabilityRepository searchAvailableByRangeAndNameAndCity(begin, end, name, city)
+      result should have size 1
+    }
+  }
+
+
+}
